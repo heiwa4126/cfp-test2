85963edc1d94fdbd6947ebbb607d8c261cede321
heiwa4126 <heiwa4126@gmail.com>
2025-04-14 13:27:36 +0900

    [WIP] just add cowsay (2)

---
+++ functions/api/cowsay.ts
@@ -0,0 +1,13 @@
+import type { ExecutionContext } from "@cloudflare/workers-types";
+import * as cowsay from "cowsay";
+
+export const onRequest = async (context: ExecutionContext): Promise<Response> => {
+	const now = new Date();
+	const output: string = cowsay.say({ text: `Moo! ${now.toISOString()}` });
+
+	return new Response(output, {
+		headers: {
+			"Content-Type": "text/plain",
+		},
+	});
+};
