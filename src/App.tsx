885aa1f270710f5311f3f090153b0cb294c01e8f
heiwa4126 <heiwa4126@gmail.com>
2025-04-15 10:51:00 +0900

    add fake 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, Route, Routes } from "react-router";
+import { Link, NavLink, Outlet, Route, Routes } from "react-router";
 import About from "./About";
 import "./App.css";
 import Home from "./Home";
@@ -21,6 +21,18 @@ function Layout1() {
 	);
 }
 
+function NotFound() {
+	return (
+		<main>
+			<h1>404</h1>
+			<p className="read-the-docs">Not Found</p>
+			<p>
+				<Link to="/">Home</Link>
+			</p>
+		</main>
+	);
+}
+
 function App() {
 	return (
 		<Routes>
@@ -28,6 +40,7 @@ function App() {
 				<Route index path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 			</Route>
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
