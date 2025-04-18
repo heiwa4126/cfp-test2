c7f864b0c9e7e9492b77cb2af22c835626172ff6
heiwa4126 <heiwa4126@gmail.com>
2025-04-18 18:17:32 +0900

    CORS test

--- functions/api/greeting.ts
+++ functions/api/greeting.ts
@@ -1,5 +1,17 @@
 import type { ExecutionContext } from "@cloudflare/workers-types";
 
+export const onRequestOptions = async (): Promise<Response> => {
+	return new Response(null, {
+		status: 204,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Allow-Methods": "GET, OPTIONS",
+			"Access-Control-Max-Age": "86400",
+		},
+	});
+};
+
 export const onRequest = async (context: ExecutionContext): Promise<Response> => {
 	return new Response(
 		JSON.stringify({
@@ -8,6 +20,8 @@ export const onRequest = async (context: ExecutionContext): Promise<Response> =>
 		{
 			headers: {
 				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Max-Age": "86400",
 			},
 		},
 	);
